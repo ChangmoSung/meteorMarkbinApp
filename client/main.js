f480e5919106efa29202b3377090fcd88e3d8186
ChangmoSung <changmosung0914@gmail.com>
2020-06-09 11:22:30 -0400

    setup

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import './main.html'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+import App from './components/app'
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.render-target'))
+})
